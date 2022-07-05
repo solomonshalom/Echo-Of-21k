34a48f28b49badfcf883585643fcaaacbd9c4e34
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2022-07-05 12:43:24 +0530

    Hello! Mom

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,16 +11,16 @@ const Carousel = ({ title }) => {
   // feeds should be fetched from external source
   // eslint-disable-next-line no-unused-vars
   const [feeds, getFeeds] = useState([
-    '/images/75095@2x.png',
-    '/images/mike-dorner-sf_1ZDA1YFw-unsplash@2x.png',
-    '/images/1683@2x.png',
-    '/images/2857@2x.png',
-    '/images/sina-katirachi-sj5nVJSfYl4-unsplash@2x.png',
-    '/images/451@2x.png',
-    '/images/markus-spiske-UJwcQ3xeFHo-unsplash@2x.png',
-    '/images/1841@2x.png',
-    '/images/15@2x.png',
-    '/images/1670@2x.png',
+    './images/75095@2x.png',
+    './images/mike-dorner-sf_1ZDA1YFw-unsplash@2x.png',
+    './images/1683@2x.png',
+    './images/2857@2x.png',
+    './images/sina-katirachi-sj5nVJSfYl4-unsplash@2x.png',
+    './images/451@2x.png',
+    './images/markus-spiske-UJwcQ3xeFHo-unsplash@2x.png',
+    './images/1841@2x.png',
+    './images/15@2x.png',
+    './images/1670@2x.png',
   ]);
 
   return (
